98bcd3b4a4e3971f6066f5f4e5194ae75ab16c2a
Michael C Smith <maxomai@gmail.com>
2020-06-18 20:01:24 -0700

    Update for 2020 OCVR files
    * Eliminated gender markers
    * Only the first word of the first name field is selected

--- OCVR-TO-CSV.bash
+++ OCVR-TO-CSV.bash
@@ -11,19 +11,17 @@
 # Copyright is at the end of the source file
 #
 # Note: this could probably run faster, but I just wanted to get it working.
+#
+# Changes 2020-June-12: removed gender detection (no longer necessary), added whitespace removal for names,
+# cut off first names after 20 characters, eliminated space between ZIP and +4
 
-MALEPCPINDICATOR="Precinct Committee Person - Male"
-FEMALEPCPINDICATOR="Precinct Committee Person - Female"
-#Note: OCVR reports did not include nonbinary gender PCPs at the time this was written.
 PRECINCTINDICATOR="Precinct :,"
 
-MALESTRING="Male"
-FEMALESTRING="Female"
 
 PRECINCT=""
-GENDERSTRING=""
 
-echo "PRECINCT,GENDER,VOTER ID,LAST NAME,FIRST NAME,MAILING STREET ADDRESS,MAILING CITY,MAILING STATE,MAILING ZIP,PHYSICAL ADDRESS,PHYSICAL CITY,PHYSICAL STATE,PHYSICAL ZIP,STATUS-GENDER,PHONE,ASSIGNMENT"
+echo "PRECINCT,VOTER ID,LAST NAME,FIRST NAME,MAILING STREET ADDRESS,MAILING CITY,MAILING STATE,MAILING ZIP,PHYSICAL ADDRESS,PHYSICAL CITY,PHYSICAL STATE,PHYSICAL ZIP,STATUS,PHONE,ASSIGNMENT"
+
 
 function parse_address() {
 	local address="$*"
@@ -33,7 +31,7 @@ function parse_address() {
 	local state=$(echo $address | rev | cut -d" " -f 2 | rev )
 	if [[ $state =~ ^[0-9]5* ]]; then
 		#process as zip+4 
-		zipcode=$(echo $address | rev | cut -d" " -f 1-2 | rev)
+		zipcode=$(echo $address | rev | cut -d" " -f 1-2 | rev | tr -d '[:space:]' )
 		street=$(echo $address | rev | cut -d" " -f5- | rev)
 		city=$(echo $address | rev | cut -d" " -f 4 | rev )
 		state=$(echo $address | rev | cut -d" " -f 3 | rev )
@@ -48,24 +46,21 @@ function parse_address() {
 
 while IFS= read line
 do
-	if [[ "$line" == *"$MALEPCPINDICATOR"* ]]; then
-		GENDERSTRING=$MALESTRING
-	elif [[ "$line" == *"$FEMALEPCPINDICATOR"* ]]; then
-		GENDERSTRING=$FEMALESTRING
-	elif [[ "$line" == *"$PRECINCTINDICATOR"* ]]; then
+	if [[ "$line" == *"$PRECINCTINDICATOR"* ]]; then
 		PRECINCT=$(echo $line | cut -d',' -f 2 | tr -d '\r');
 	elif [[ "$line" =~ ^[0-9]6* ]]; then
-		id_and_name=$(echo $line | cut -d"," -f 1-3);
+		id_and_lastname=$(echo $line | cut -d"," -f 1-2);
+		firstname=$(echo $line | cut -d"," -f 3 | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' | cut -d" " -f 1 | cut -c -20);
 		ma_string=$(echo $line | cut -d"," -f 4);
 		ma_parsed=$(parse_address $ma_string);
 		pa_string=$(echo $line | cut -d"," -f 5);
 		pa_parsed=$(parse_address $pa_string);
 		the_rest=$(echo $line | cut -d"," -f 6-);
-		echo "$PRECINCT,$GENDERSTRING,$id_and_name,$ma_parsed,$pa_parsed,$the_rest"
+		echo "$PRECINCT,$id_and_lastname,$firstname,$ma_parsed,$pa_parsed,$the_rest"
 	fi
 done < "${1:-/dev/stdin}"
 
-#Copyright 2018 Michael C Smith (maxomai@gmail.com), Technology Officer, Democratic Party of Multnomah County
+#Copyright 2018-2020, Michael C Smith (maxomai@gmail.com), Second Vice-Chair, Democratic Party of Multnomah County
 #
 #Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 #
